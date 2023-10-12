d2844e4607db5209a2e1f73c2f81c687d8184aed
Karol Denst <kdenst@gmail.com>
2023-10-12 18:08:18 +0200

    Initial commit

---
+++ GraphAlgorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿using GraphAlgorithms;
+
+var clique = GraphFactory.CreateClique(10);
+var found = LargestCliqueApproximator.Find(clique);
+Console.WriteLine($"Clique size: {clique.Size}, found: {found.Vertices.Count}, edges: {found.EdgeCount}");
+
+for (int i = 0; i < 100; i++)
+{
+    var cliqueSize = 8;
+    var random = GraphFactory.CreateRandomWithClique(30, 300, cliqueSize, i);
+    found = LargestCliqueApproximator.Find(random);
+    Console.WriteLine($"Min clique size: {cliqueSize}, found: {found.Vertices.Count}, edges: {found.EdgeCount}");
+}
