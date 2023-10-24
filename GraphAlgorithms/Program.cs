e29be0a8e3033b97a64e11cccb74dbdec6a130f0
Bartosz Dybowski <01161292@pw.edu.pl>
2023-10-24 20:09:33 +0200

    feature: added naive algorithms

--- GraphAlgorithms/Program.cs
+++ GraphAlgorithms/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GraphAlgorithms;
+using System;
+
+
+var clique = GraphFactory.CreateClique(10);
+var found = LargestCliqueApproximator.Find(clique);
+Console.WriteLine($"Clique size: {clique.Size}, found: {found.Vertices.Count}, edges: {found.EdgeCount}");
+
+for (int i = 0; i < 100; i++)
+{
+    var cliqueSize = 8;
+    var random = GraphFactory.CreateRandomWithClique(30, 300, cliqueSize, i);
+    found = LargestCliqueApproximator.Find(random);
+    Console.WriteLine($"Min clique size: {cliqueSize}, found: {found.Vertices.Count}, edges: {found.EdgeCount}");
+}
---
+++ GraphAlgorithmsTests/LargestCliqueNaiveFinderTests.cs
@@ -0,0 +1,213 @@
+﻿using GraphAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GraphAlgorithms.Tests
+{
+    [TestFixture]
+    public class LargestCliqueNaiveFinderTests
+    {
+        private static IEnumerable<TestCaseData> TestCases()
+        {
+            Graph graph;
+            // graphs with no edges
+            graph = new Graph(1);
+            yield return new TestCaseData(graph, new List<int> { 0 });
+
+            graph = new Graph(5);
+            yield return new TestCaseData(graph, new List<int> { 0 });
+
+            graph = new Graph(10);
+            yield return new TestCaseData(graph, new List<int> { 0 });
+
+            // graphs that are clique
+            graph = new Graph(2);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(1, 0);
+            yield return new TestCaseData(graph, new List<int> { 0, 1 });
+
+            graph = new Graph(5);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 1, 1, 1 },
+                new int[] { 1, 0, 1, 1, 1 },
+                new int[] { 1, 1, 0, 1, 1 },
+                new int[] { 1, 1, 1, 0, 1 },
+                new int[] { 1, 1, 1, 1, 0 },
+            };
+            yield return new TestCaseData(graph, new List<int> { 0, 1, 2, 3, 4 });
+
+            graph = new Graph(10);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 0, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 0, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }
+            };
+            yield return new TestCaseData(graph, new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+            // graph with max clique of size 1
+            graph = new Graph(5);
+            yield return new TestCaseData(graph, new List<int> { 0 });
+
+            // graph with many cliques of size 2
+            graph = new Graph(8);
+            for(int i = 0; i < 8; i += 2)
+            {
+                graph.AddEdge(i, i + 1);
+                graph.AddEdge(i + 1, i);
+            }
+            yield return new TestCaseData(graph, new List<int> { 0, 1 });
+
+            // graphs with edges that not belong to any cliques
+            graph = new Graph(10);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 },
+                new int[] { 1, 0, 0, 0, 1, 0, 1, 1, 0, 0 },
+                new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int[] { 1, 1, 0, 0, 0, 0, 1, 1, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 1, 0, 1, 1, 0, 0, 0, 1, 0 },
+                new int[] { 1, 1, 0, 0, 1, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0 },
+                new int[] { 1, 0, 0, 1, 0, 0, 0, 0, 1, 0 }
+            };
+            yield return new TestCaseData(graph, new List<int> { 0, 1, 4, 7 });
+
+            graph = new Graph(10);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 0, 0, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 },
+                new int[] { 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }
+            };
+            yield return new TestCaseData(graph, new List<int> { 0, 1, 2, 3, 4, 7, 8, 9});
+
+
+            graph = new Graph(10);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 0, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 0, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 }
+            };
+            yield return new TestCaseData(graph, Enumerable.Range(0, 9).ToList());
+
+            // many equal cliques
+            graph = new Graph(6);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 1, 0, 1, 1 },
+                new int[] { 1, 0, 1, 1, 0, 1 },
+                new int[] { 1, 1, 0, 1, 1, 0 },
+                new int[] { 0, 1, 1, 0, 1, 1 },
+                new int[] { 1, 0, 1, 1, 0, 1 },
+                new int[] { 1, 1, 0, 1, 1, 0 },
+            };
+            yield return new TestCaseData(graph, new List<int> { 0, 1, 2 });
+
+            //
+            // DEF: MAX_CLIQUE is number of vertices from largest clique
+            // DEF: deg = deg + 1 when there are edges in both sides between vertices
+            // graphs with vertices 'v' that deg(v) >= MAX_CLIQUE, but don't expand the biggest clique
+            //
+
+            // 1) v = 3: 3 = deg(v) == MAX_CLIQUE = 3
+            graph = new Graph(5);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 1, 1, 0 },
+                new int[] { 1, 0, 1, 1, 0 },
+                new int[] { 1, 1, 0, 1, 0 },
+                new int[] { 1, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 1, 0 },
+            };
+            yield return new TestCaseData(graph, new List<int> { 0, 1, 2 });
+
+            // 2) v = 3 : 4 = deg(v) > MAX_CLIQUE = 3
+            graph = new Graph(6);
+            graph.AdjacencyMatrix = new int[][] {
+                new int[] { 0, 1, 1, 1, 0, 0 },
+                new int[] { 1, 0, 1, 0, 0, 0 },
+                new int[] { 1, 1, 0, 1, 0, 0 },
+                new int[] { 1, 0, 1, 0, 1, 1 },
+                new int[] { 0, 0, 0, 1, 0, 0 },
+                new int[] { 0, 0, 0, 1, 0, 0 },
+            };
+            yield return new TestCaseData(graph, new List<int> { 0, 1, 2 });
+
+            // some random graphs, graphs in which vertices 'v' that don't belong to clique have
+            // deg(v) < MAX_CLIQUE
+            var random = new Random();
+            int maxSize = 25;
+            for (int i = 0; i < 100; ++i)
+            {
+                graph = new Graph(random.Next(1, maxSize));
+                int cliqueSize = random.Next(1, graph.Size);
+                var clique = GenerateClique(graph, cliqueSize);
+                GenerateAdditionalVertices(graph, clique);
+                yield return new TestCaseData(graph, clique);
+            }
+        }
+
+        private static List<int> GenerateClique(Graph graph, int cliqueSize)
+        {
+            if (cliqueSize == 1)
+                return new List<int>() { 0 };
+            Random random = new Random();
+            List<int> clique = new List<int>();
+            while (clique.Count < cliqueSize)
+            {
+                int u = random.Next(graph.Size);
+                if (clique.Contains(u))
+                    continue;
+                foreach (var v in clique)
+                    graph.AddBothSidesEdge(u, v);
+                clique.Add(u);
+            }
+            return clique;
+        }
+
+        private static void GenerateAdditionalVertices(Graph graph, List<int> clique)
+        {
+            Random random = new Random();
+            int add = random.Next(graph.Size);
+            for (int i = 0; i < add; ++i)
+            {
+                int u = random.Next(graph.Size);
+                int v = random.Next(graph.Size);
+                if (graph.GetDegree(u) < clique.Count - 1 && graph.GetDegree(v) < clique.Count - 1)
+                    graph.AddEdge(u, v);
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void FindTest(Graph graph, List<int> expectedResult)
+        {
+            List<int> result = new();
+            LargestCliqueNaiveFinder.Find(graph, graph.Size, new List<int>(), 0, ref result);
+            Assert.That(result, Is.EquivalentTo(expectedResult));
+        }
+    }
+}
