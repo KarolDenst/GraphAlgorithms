2319c53f159211d0b4eb34905fcabdcac3f2f08a
Bartosz Dybowski <01161292@pw.edu.pl>
2023-10-24 19:06:20 +0200

    fix: deleted unused code

--- GraphAlgorithms/Program.cs
+++ GraphAlgorithms/Program.cs
@@ -13,19 +13,3 @@ for (int i = 0; i < 100; i++)
     found = LargestCliqueApproximator.Find(random);
     Console.WriteLine($"Min clique size: {cliqueSize}, found: {found.Vertices.Count}, edges: {found.EdgeCount}");
 }
-
-
-
-var result = LargestCommonSubgraphNaiveFinder.GenerateColumnsPermutations(new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } });
-for(int i = 0; i < result.Count; i++)
-{
-    for (int j = 0; j < result[i].Length; j++)
-    {
-        for (int k = 0; k < result[i][j].Length; k++)
-        {
-            Console.Write(result[i][j][k] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
